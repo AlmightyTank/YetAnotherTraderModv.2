5ca5d207a53ebb78c70edaca7a16eeaaaef6add0
AMightyTank <chrisyoungplus@outlook.com>
2026-06-17 09:46:03 -0500

    Update TonyQuestLoader.cs
    Add Custom Locales Support

--- TonyQuestLoader.cs
+++ TonyQuestLoader.cs
@@ -15,7 +15,7 @@ public class TonyQuestLoader(WTTServerCommonLib.WTTServerCommonLib wttCommon) : 
         Assembly assembly = Assembly.GetExecutingAssembly();
 
         await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
-        //await wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
+        await wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
         await wttCommon.CustomLootspawnService.CreateCustomLootSpawns(assembly);
         await wttCommon.CustomQuestService.CreateCustomQuests(assembly);
         await wttCommon.CustomQuestZoneService.CreateCustomQuestZones(assembly);
